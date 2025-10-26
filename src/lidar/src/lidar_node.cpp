292344968ff9f37c34a3aac1bba088f352f9e7fb
Justin Gardiner <justinpgardiner@gmail.com>
2025-10-26 17:46:22 -0400

    fixed lidarnode bugs

--- src/lidar/src/lidar_node.cpp
+++ src/lidar/src/lidar_node.cpp
@@ -1,16 +1,22 @@
+#include <iostream>
+
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+
+namespace lidar {
 
 class LidarNode : public rclcpp::Node
 {
 public:
-    LidarNode(const rclcpp::NodeOptions &options) : Node("lidar_node", options)
+    LidarNode(const rclcpp::NodeOptions &options) : Node("lidarnode", options)
     {
         dummy_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(100),  // 10 Hz
+            std::chrono::milliseconds(10),  // 10 Hz
             std::bind(&LidarNode::publish_odom, this)
         );
+        // std::cout << "Started" << std::endl;
     }
 private:
     void publish_odom()
@@ -39,9 +45,17 @@ private:
         msg.twist.twist.angular.y = 0.0;
         msg.twist.twist.angular.z = 0.0;
 
+        // std::cout << "Publishing\n";
+
         dummy_pub_->publish(msg);
+
+        
     }
 
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr dummy_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
+
+RCLCPP_COMPONENTS_REGISTER_NODE(lidar::LidarNode)
+
+}
